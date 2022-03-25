e7813966f45f1306c2689ae380989e29ace4268c
Ansh Sikka <sikka008@umn.edu>
2022-03-24 20:16:21 -0500

    Chrome extension works on Assignment upload pages. Listens for clicks to 'Submit Assignment' and brings up a dialog box accordingly.

--- content.js
+++ content.js
@@ -1,2 +1,11 @@
 var courses_button_text = document.getElementById('global_nav_courses_link').getElementsByTagName("div")[1]
 courses_button_text.innerHTML = "Classes"
+
+var submit_assignment_button = document.getElementById('submit_file_button')
+submit_assignment_button.addEventListener('click', function(){
+    var myDialog = document.createElement("dialog");
+    document.body.appendChild(myDialog)
+    var text = document.createTextNode("This is a dialog window");
+    myDialog.appendChild(text);
+    myDialog.showModal();
+})
